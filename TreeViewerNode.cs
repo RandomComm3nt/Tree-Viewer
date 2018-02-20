99ca8b34da5731d2ae82d27c06ca0419f3bd1e96
RandomComm3nt <RandomComm3nt@hotmail.co.uk>
2018-02-20 22:17:05 +0000

    #134 - Started on node viewer tools

---
+++ TreeViewerNode.cs
@@ -0,0 +1,64 @@
+﻿
+#region Using Statements
+using UnityEditor;
+using UnityEngine;
+#endregion
+
+namespace Assets.Scripts.Editor.TreeViewer
+{
+	public class TreeViewerNode
+	{
+		#region Fields
+
+		public static Color backgroundColour = Color.blue;
+		public static Color outlineColour = Color.black;
+
+		private float x;
+		private float y;
+		private float width;
+		private float height;
+
+		private bool mouseDown;
+
+		#endregion
+
+		#region Methods
+
+		public TreeViewerNode()
+		{
+			x = 0;
+			y = 0;
+			width = 100;
+			height = 100;
+		}
+
+		public void Draw()
+		{
+			Handles.DrawSolidRectangleWithOutline(new Rect(x, y, width, height), backgroundColour, outlineColour);
+		}
+
+		public bool HandleMouseEvent()
+		{
+			if (new Rect(x, y, width, height).Contains(Event.current.mousePosition))
+			{
+				if (Event.current.type == EventType.MouseDown)
+				{
+					mouseDown = true;
+				}
+				else if (Event.current.type == EventType.MouseUp)
+				{
+					mouseDown = false;
+				}
+				return true;
+			}
+			return false;
+		}
+
+		public void MouseUpOutside()
+		{
+			mouseDown = false;
+		}
+
+		#endregion
+	}
+}
---
+++ TreeViewerWindow.cs
@@ -0,0 +1,80 @@
+﻿
+#region Using Statements
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+#endregion
+
+namespace Assets.Scripts.Editor.TreeViewer
+{
+	public class TreeViewerWindow : EditorWindow
+	{
+		#region Fields
+
+		private bool init = false;
+		private List<TreeViewerNode> nodes;
+
+		#endregion
+
+		#region Methods
+
+		[MenuItem("Window/Tree Viewer")]
+		internal static void Init()
+		{
+			// Get existing open window or if none, make a new one
+			TreeViewerWindow window = GetWindow<TreeViewerWindow>();
+			window.Show();
+		}
+
+		private void Initiate()
+		{
+			nodes = new List<TreeViewerNode>();
+			nodes.Add(new TreeViewerNode());
+		}
+
+		private void OnGUI()
+		{
+			if (!init)
+				Initiate();
+			HandleEvents();
+			foreach (TreeViewerNode node in nodes)
+			{
+				node.Draw();
+			}
+		}
+
+		private void HandleEvents()
+		{
+			// if active event, find node under mouse and handle event
+			if (Event.current != null)
+			{
+				bool eventHandled = false;
+				foreach (TreeViewerNode node in nodes)
+				{
+					// only find first node to handle event
+					if (node.HandleMouseEvent())
+					{
+						eventHandled = true;
+						break;
+					}
+					// if not handled by node, handle as background click
+					if (!eventHandled)
+						HandleEventOnBackground();
+				}
+			}
+		}
+
+		private void HandleEventOnBackground()
+		{
+			if (Event.current.type == EventType.MouseUp)
+			{
+				foreach (TreeViewerNode node in nodes)
+				{
+					node.MouseUpOutside();
+				}
+			}
+		}
+
+		#endregion
+	}
+}
